c0541d22bd44ba5721a2ce4f80fe304a0fd0859c
ushantsingh <ushantsingh4@gmail.com>
2025-09-02 15:55:14 +0530

    msg

---
+++ tut5.c
@@ -0,0 +1,16 @@
+//eben odd 
+#include<stdio.h>
+void main()
+{
+    int n;
+    prinf("Enter a nuber :");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("The number is even ");
+    }
+      else
+    { 
+        printf("The number is odd ");   
+    }
+}    
