1fa8451f1f9f496c1cfbab442ee2b064d2e1d490
ushantsingh <ushantsingh4@gmail.com>
2025-09-08 14:58:39 +0530

    pattern square type

--- pattern.cpp
+++ pattern.cpp
@@ -7,12 +7,13 @@ int main()
 {
     for(int i=0;i<5;i++)
     {
-        cout<<"\n*"<<endl;
+        //cout<<"\n*"<<endl;
         for(int j=0;j<5;j++)
         {
             
-            cout<<"*"<<endl;
+            cout<<"*";
         }
+        cout<<endl;
     }
     return 0;
 }
