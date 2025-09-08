6e7a04c8aeb036f9a35267b4391f07e8e7dad952
ushantsingh <ushantsingh4@gmail.com>
2025-09-08 14:54:58 +0530

    pattern

---
+++ pattern.cpp
@@ -0,0 +1,18 @@
+//PATERN
+
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    for(int i=0;i<5;i++)
+    {
+        cout<<"\n*"<<endl;
+        for(int j=0;j<5;j++)
+        {
+            
+            cout<<"*"<<endl;
+        }
+    }
+    return 0;
+}
