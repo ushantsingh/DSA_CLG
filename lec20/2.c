9c2a04c5aa599bdf95ed70dcdc1e79bc4df36b05
Ushant Singh <ushantsingh4@gmail.com>
2025-10-09 15:40:56 +0530

    Fixed lec20 — removed submodule, added as normal folder

---
+++ lec20/2.c
@@ -0,0 +1,21 @@
+// REVERSING A STRING
+
+
+#include <stdio.h>
+
+int main() {
+    char str[] = "hii";
+    int length = 0;
+
+    // Find the length of the string manually
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    // Print the string in reverse order
+    for (int i = length - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
---
+++ lec20/4.c
@@ -0,0 +1,27 @@
+// palindrome
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[]="madam";
+    int ln=strlen(str);
+    int isTrue=1;
+    for(int i=0;i<ln/2;i++)
+    {
+        if(str[i]!=str[ln-i-1])
+        {
+            isTrue=0;
+            break;
+        }
+    }
+    if(isTrue!=0)
+    {
+        printf("It is Palindrome");
+    }
+    else
+    {
+        printf("Not Palindrome");
+    }
+}
