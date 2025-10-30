5b4fd605698a694fd743047b7a745a3f68b7d604
Ushant Singh <ushantsingh4@gmail.com>
2025-10-30 15:33:54 +0530

    FIbonacci series

---
+++ lec29/4.c
@@ -0,0 +1,23 @@
+// FIbonacci series 
+
+#include<stdalign.h>
+int fibonacci(int a)
+{
+    if(a==0)
+    {
+        return 0;
+    }
+    else if(a==1)
+    {
+        return 1;
+    }
+    else 
+    {
+        return fibonacci(a - 1) + fibonacci(a - 2);
+    }
+}
+
+int main(){
+    int data = fibonacci(8);
+    printf("%d", data);
+}
